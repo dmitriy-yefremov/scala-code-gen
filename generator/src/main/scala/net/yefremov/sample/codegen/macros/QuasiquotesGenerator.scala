ba7da3f973a7d1099b8ddba4d9bc374242ebb86a
Dmitriy Yefremov <dyefremov@linkedin.com>
2014-12-26 14:21:57 -0800

    macro generator code cleanup

--- generator/src/main/scala/net/yefremov/sample/codegen/macros/QuasiquotesGenerator.scala
+++ generator/src/main/scala/net/yefremov/sample/codegen/macros/QuasiquotesGenerator.scala
@@ -1,12 +1,10 @@
 package net.yefremov.sample.codegen.macros
 
-import net.yefremov.sample.codegen.schema.FieldType._
-
 import scala.annotation.StaticAnnotation
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
-import net.yefremov.sample.codegen.schema.{FieldType, TypeSchema}
+import net.yefremov.sample.codegen.schema.TypeSchema
 
 /**
  * A macro generator based on quasiquates.
@@ -18,35 +16,38 @@ object QuasiquotesGenerator {
 
     import c.universe._
 
-    // retrieve the schema file path from the code tree
-    def getSchemaPath: String = {
-      c.prefix.tree match {
-        case Apply(_, List(Literal(Constant(x)))) => x.toString
-        case _ => c.abort(c.enclosingPosition, "schema file path is not specified")
-      }
+    // retrieve the schema path
+    val schemaPath = c.prefix.tree match {
+      case Apply(_, List(Literal(Constant(x)))) => x.toString
+      case _ => c.abort(c.enclosingPosition, "schema file path is not specified")
     }
 
-    annottees.map(_.tree) match {
-      case List(q"class $name") =>
+    // retrieve the annotate class name
+    val className = annottees.map(_.tree) match {
+      case List(q"class $name") => name
+      case _ => c.abort(c.enclosingPosition, "the annotation can only be used with classes")
+    }
 
-        val schema = TypeSchema.fromJson(getSchemaPath)
+    // load the schema from JSON
+    val schema = TypeSchema.fromJson(schemaPath)
 
-        val params = schema.fields.map { field =>
-          val fieldName = newTermName(field.name)
-          val fieldType = newTypeName(field.valueType.toString)
-          q"val $fieldName: $fieldType"
+    // produce the list of constructor parameters (note the "val ..." syntax)
+    val params = schema.fields.map { field =>
+      val fieldName = newTermName(field.name)
+      val fieldType = newTypeName(field.valueType.toString)
+      q"val $fieldName: $fieldType"
+    }
+
+    // rewrite the class definition
+    c.Expr(
+      q"""
+        case class $className(..$params) {
+
+          def schema = ${schema.toString}
+
         }
-
-        c.Expr[Any](
-          q"""
-            case class $name(..$params) {
-
-              def schema = ${schema.toString}
-
-            }
-          """
-        )
-    }
+      """
+    )
   }
 
 }
